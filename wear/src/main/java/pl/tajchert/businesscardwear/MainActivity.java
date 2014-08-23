4f09db1d807b464e76091805174ea7b1d7330124
Michal Tajchert <mtajchert@gmail.com>
2014-08-23 23:25:26 +0200

    Working build.

--- wear/src/main/java/pl/tajchert/businesscardwear/MainActivity.java
+++ wear/src/main/java/pl/tajchert/businesscardwear/MainActivity.java
@@ -1,12 +1,18 @@
 package pl.tajchert.businesscardwear;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class MainActivity extends Activity {
-    private ImageView qrCode;
+    private ImageView imageViewQRCode;
+    private String imagePath = Environment.getExternalStorageDirectory() + ValuesCons.WEAR_IMAGE_PATH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +22,26 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                qrCode = (ImageView) stub.findViewById(R.id.image_qr_code);
+                imageViewQRCode = (ImageView) stub.findViewById(R.id.image_qr_code);
+                setImageQRCode();
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void setImageQRCode(){
+        File file = new File(imagePath);
+        if(file.exists()){
+            Bitmap qrCode = readBitmapFromFile();
+            if(qrCode != null && imageViewQRCode != null) {
+                imageViewQRCode.setImageBitmap(qrCode);
+            }
+        }
+    }
+
+
+    private Bitmap readBitmapFromFile(){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        return BitmapFactory.decodeFile(imagePath, options);
     }
 }
