05ce82b39a5b288a8262443643526b429f1281dd
Michal Tajchert <mtajchert@gmail.com>
2014-08-24 01:04:28 +0200

    Fixes, added tip and save button.

--- mobile/src/main/java/pl/tajchert/wear/businesscard/ui/MainActivity.java
+++ mobile/src/main/java/pl/tajchert/wear/businesscard/ui/MainActivity.java
@@ -37,7 +37,8 @@ public class MainActivity extends FragmentActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_save) {
+            onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
